bf7f9ae2d54afc4095d1d73d97468c768f3139c5
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-11-08 11:53:25 +0000

    step 4

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
