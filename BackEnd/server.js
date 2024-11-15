e36d605b4a9e4871d9cf7ff84ff9f8b559080175
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-11-15 11:49:44 +0000

    Implement a method to fetch all movie records

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -65,32 +65,36 @@ app.post('/api/movies', async (req, res)=>{
     res.status(201).json({ message: 'Movie created successfully', movie: newMovie });
     })
 
+    app.get('/api/movies', async (req, res) => {
+        const movies = await Movie.find({});
+        res.json(movies);
+      });
 
 // movies route to return data in json format
-app.get('/api/movies', (req, res) => {
-    const movies = [
-        {
-            "Title": "Avengers: Infinity War (server)",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War (server)",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "World War Z (server)",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
-    ];
-    res.status(200).json({ myMovies: movies });
+// app.get('/api/movies', (req, res) => {
+//     const movies = [
+//         {
+//             "Title": "Avengers: Infinity War (server)",
+//             "Year": "2018",
+//             "imdbID": "tt4154756",
+//             "Type": "movie",
+//             "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+//         },
+//         {
+//             "Title": "Captain America: Civil War (server)",
+//             "Year": "2016",
+//             "imdbID": "tt3498820",
+//             "Type": "movie",
+//             "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+//         },
+//         {
+//             "Title": "World War Z (server)",
+//             "Year": "2013",
+//             "imdbID": "tt0816711",
+//             "Type": "movie",
+//             "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+//         }
+//     ];
+    // res.status(200).json({ myMovies: movies });
     // res.json({ movies });
-});
+// });
